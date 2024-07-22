9e4ba1a9666ba4e8cbb64ff5f5aac47e9b35a557
MananSharma12 <manan.sharma1205@gmail.com>
2024-07-22 23:35:02 +0530

    feat: add routes, prisma
    - Added basic routes
    - Added prisma
    - Basic DB setup and migrations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,29 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
+const apiV1 = new Hono()
 
-app.get('/', (c) => {
+app.route('/api/v1', apiV1)
+
+apiV1.post('/user/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+apiV1.post('/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+apiV1.post('/blog', (c) => {
+  return c.text('Hello Hono!')
+}).put((c) => {
+  return c.text('Hello Hono!')
+})
+
+apiV1.get('/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+apiV1.get('/blog/bulk', (c) => {
   return c.text('Hello Hono!')
 })
 
