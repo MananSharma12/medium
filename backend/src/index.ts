d472d23275ad5a666f47553fea8f67334155cabf
MananSharma12 <manan.sharma1205@gmail.com>
2024-12-01 16:44:42 +0530

    bug: fix syntax, prisma upgrade

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,11 @@
 import {Hono} from 'hono'
-import {PrismaClient} from '@prisma/client'
+import {PrismaClient} from '@prisma/client/edge'
 import {withAccelerate} from "@prisma/extension-accelerate"
 
 const app = new Hono()
-const apiV1 = new Hono<{ Bindings: { DATABASE_URL: string } }>()
 
-app.route('/api/v1', apiV1)
-
-apiV1.post('/user/signup', async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL
-    }).$extends(withAccelerate())
+app.post('/api/v1/user/signup', async (c) => {
+    const prisma = new PrismaClient().$extends(withAccelerate())
 
     const body = await c.req.json();
     const {email, password} = body
@@ -25,21 +20,21 @@ apiV1.post('/user/signup', async (c) => {
     return c.text('Hello Hono!')
 })
 
-apiV1.post('/user/signin', (c) => {
+app.post('/api/v1/user/signin', (c) => {
     return c.text('Hello Hono!')
 })
 
-apiV1.post('/blog', (c) => {
+app.post('/api/v1/blog', (c) => {
     return c.text('Hello Hono!')
 }).put((c) => {
     return c.text('Hello Hono!')
 })
 
-apiV1.get('/blog/:id', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
     return c.text('Hello Hono!')
 })
 
-apiV1.get('/blog/bulk', (c) => {
+app.get('/api/v1/blog/bulk', (c) => {
     return c.text('Hello Hono!')
 })
 
